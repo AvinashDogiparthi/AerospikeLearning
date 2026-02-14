f6837d807fd31cea840c704138c01366683baf11
AvinashDogiparthi <dvsubharao1963@gmail.com>
2026-02-14 19:07:16 +0530

    Initial project setup

---
+++ src/test/java/com/learning/aerospike/AerospikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.aerospike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AerospikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
